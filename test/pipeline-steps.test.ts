8c560878196cef21de3402b73e6cd83ba33ac920
David Yao <yaodavidarmel@gmail.com>
2023-02-13 17:33:04 -0600

    added more tests

--- test/pipeline-steps.test.ts
+++ test/pipeline-steps.test.ts
@@ -1,10 +1,11 @@
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, test, afterAll } from "@jest/globals";
 import { extractFilesetFromPath } from "../src/cli/pipeline-steps";
 import fs from "fs";
 
 describe("extractFilesetFromPath", () => {
+  const path = "./test/fixtures";
+  const filename = `${path}/example.txt`;
   test("should return empty array when given an empty directory path", () => {
-    const path = "./test/fixtures";
     fs.mkdirSync(`${path}/empty-directory`, { recursive: true });
     const result = extractFilesetFromPath(path);
     expect(result).toEqual([]);
@@ -13,6 +14,24 @@ describe("extractFilesetFromPath", () => {
   test("should return a non-empty array when given a non-empty directory path", () => {
     const path = "./test/fixtures/manifest-only-package";
     const result = extractFilesetFromPath(path);
+    expect(result.length).toEqual(2);
+    expect(result[0].path).toEqual(
+      "test/fixtures/manifest-only-package/Manifest.ocf.json"
+    );
+  });
+
+  test("file contains 'hello, world'", () => {
+    const expectedContent = "hello, world";
+    fs.writeFileSync(filename, expectedContent);
+    const result = extractFilesetFromPath(path);
     expect(result.length).toBeGreaterThan(0);
+    const actualContent = result[0].readAsText();
+    expect(actualContent).toBe(expectedContent);
+    expect(result[0].path).toEqual("test/fixtures/example.txt");
+  });
+
+  afterAll(() => {
+    fs.rmdirSync(`${path}/empty-directory`, { recursive: true });
+    fs.unlinkSync(filename);
   });
 });
