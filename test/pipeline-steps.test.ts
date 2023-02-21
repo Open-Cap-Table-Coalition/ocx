4ffdcda4a6cc3a71128e3bade844b56a0c22f16f
Patrick Johnmeyer <patrick@ltse.com>
2023-02-21 10:17:58 -0600

    Restructure pipeline-steps tests a bit (#4)

--- test/pipeline-steps.test.ts
+++ test/pipeline-steps.test.ts
@@ -3,35 +3,71 @@ import { extractFilesetFromPath } from "../src/cli/pipeline-steps";
 import fs from "fs";
 
 describe("extractFilesetFromPath", () => {
-  const path = "./test/fixtures";
-  const filename = `${path}/example.txt`;
+  const exampleFilePath = tempfilePath("example.txt");
+  const emptyDirectoryPath = tempfilePath("empty-directory");
+
   test("should return empty array when given an empty directory path", () => {
-    fs.mkdirSync(`${path}/empty-directory`, { recursive: true });
-    const result = extractFilesetFromPath(path);
+    fs.mkdirSync(emptyDirectoryPath, { recursive: true });
+    const result = extractFilesetFromPath(emptyDirectoryPath);
     expect(result).toEqual([]);
   });
 
   test("should return a non-empty array when given a non-empty directory path", () => {
-    const path = "./test/fixtures/manifest-only-package";
-    const result = extractFilesetFromPath(path);
-    expect(result.length).toEqual(2);
+    const result = extractFilesetFromPath(fixturePath("manifest-only-package"));
+    expect(result.length).toEqual(1);
     expect(result[0].path).toEqual(
       "test/fixtures/manifest-only-package/Manifest.ocf.json"
     );
   });
 
-  test("file contains 'hello, world'", () => {
-    const expectedContent = "hello, world";
-    fs.writeFileSync(filename, expectedContent);
-    const result = extractFilesetFromPath(path);
-    expect(result.length).toBeGreaterThan(0);
-    const actualContent = result[0].readAsText();
-    expect(actualContent).toBe(expectedContent);
-    expect(result[0].path).toEqual("test/fixtures/example.txt");
+  test("should handle a directory with multiple files", () => {
+    const result = extractFilesetFromPath(
+      fixturePath("ambiguous-manifest-package")
+    );
+
+    expect(result.length).toBe(2);
+    expect(
+      result.find(
+        (f) =>
+          f.path == fixturePath("ambiguous-manifest-package/Manifest.ocf.json")
+      )
+    ).not.toBeNull();
+    expect(
+      result.find(
+        (f) =>
+          f.path ==
+          fixturePath("ambiguous-manifest-package/ManifestClone.ocf.json")
+      )
+    ).not.toBeNull();
+
+    // ensure that each file in the result set is linked with the correct
+    // contents
+    result.forEach((file) => {
+      expect(file.readAsText()).toEqual(fs.readFileSync(file.path, "utf8"));
+    });
   });
 
+  test("file contains 'hello, world'", () => {
+    const expectedContent = "hello, world";
+    fs.writeFileSync(exampleFilePath, expectedContent);
+    const result = extractFilesetFromPath(tempfilePath(""));
+    expect(result.length).toBeGreaterThan(0);
+
+    const myFile = result.find((r) => r.path === exampleFilePath);
+    const actualContent = myFile?.readAsText();
+    expect(actualContent).toBe(expectedContent);
+  });
+
+  function fixturePath(fixtureName: string) {
+    return `test/fixtures/${fixtureName}`;
+  }
+
+  function tempfilePath(pathName: string) {
+    return `tmp/${pathName}`;
+  }
+
   afterAll(() => {
-    fs.rmdirSync(`${path}/empty-directory`, { recursive: true });
-    fs.unlinkSync(filename);
+    fs.rmdirSync(emptyDirectoryPath, { recursive: true });
+    fs.unlinkSync(exampleFilePath);
   });
 });
