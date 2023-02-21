d0914a74474faf8a4c0fb215068f9c3c6bc2986b
Patrick Johnmeyer <patrick@ltse.com>
2023-02-21 09:57:44 -0600

    Separate one and two file extractFilesetFromPath tests

--- test/pipeline-steps.test.ts
+++ test/pipeline-steps.test.ts
@@ -14,10 +14,37 @@ describe("extractFilesetFromPath", () => {
 
   test("should return a non-empty array when given a non-empty directory path", () => {
     const result = extractFilesetFromPath(fixturePath("manifest-only-package"));
-    expect(result.length).toEqual(2);
+    expect(result.length).toEqual(1);
     expect(result[0].path).toEqual(
       "test/fixtures/manifest-only-package/Manifest.ocf.json"
     );
+  });
+
+  test("should handle a directory with multiple files", () => {
+    const result = extractFilesetFromPath(
+      fixturePath("ambiguous-manifest-package")
+    );
+
+    expect(result.length).toBe(2);
+    expect(
+      result.find(
+        (f) =>
+          f.path == fixturePath("ambiguous-manifest-package/Manifest.ocf.json")
+      )
+    ).not.toBeNull();
+    expect(
+      result.find(
+        (f) =>
+          f.path ==
+          fixturePath("ambiguous-manifest-package/ManifestClone.ocf.json")
+      )
+    ).not.toBeNull();
+
+    // ensure that each file in the result set is linked with the correct
+    // contents
+    result.forEach((file) => {
+      expect(file.readAsText()).toEqual(fs.readFileSync(file.path, "utf8"));
+    });
   });
 
   test("file contains 'hello, world'", () => {
