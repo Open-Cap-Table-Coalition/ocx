Open-Cap-Table-Coalition/ocx

commit 1d0a7e77900f03c73817042f7188a818cd3a8ae0

Remove need for ../ relative imports

src/cli/index.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
-import { version } from "../../package.json";
+import { version } from "package.json";
 
 import Excel from "exceljs";
-import ExcelJSWriter from "../workbook/exceljs-writer";
+import ExcelJSWriter from "src/workbook/exceljs-writer";
 
-import OCX from "../";
+import OCX from "src";
 
 import * as pipeline from "./pipeline-steps";
 

jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  moduleNameMapper: {
+    "^src((/.*)|)$": "<rootDir>/src$1",
+    "^test((/.*)|)$": "<rootDir>/test$1",
+  },
   modulePathIgnorePatterns: ["<rootDir>/dist/"],
   preset: "ts-jest",
   testEnvironment: "node",

test/model.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from "@jest/globals";
 
-import OCX from "../src";
+import OCX from "src";
 
 describe("model", () => {
   test("creation", () => {

test/package.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from "@jest/globals";
 
 import { extractFilesetFromPath } from "../src/cli/pipeline-steps";
 
-import OCX from "../src";
+import OCX from "src";
 
 describe("ocf-package", () => {
   function mockManifestFile(attrOverrides: object) {

test/pipeline-steps.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test, afterAll } from "@jest/globals";
-import { extractFilesetFromPath } from "../src/cli/pipeline-steps";
+import { extractFilesetFromPath } from "src/cli/pipeline-steps";
 import fs from "fs";
 
 describe("extractFilesetFromPath", () => {

test/workbook.test.ts
@@ -2,8 +2,8 @@ import { describe, expect, test } from "@jest/globals";
 
 import Excel from "exceljs";
 
-import OCX from "../src";
-import ExcelJSWriter from "../src/workbook/exceljs-writer";
+import OCX from "src";
+import ExcelJSWriter from "src/workbook/exceljs-writer";
 
 describe("workbook", () => {
   const fakeModel = {

commit 103b0bb9405c7700da209c2fe8fb087de87c7d9e

Fix bug running CLI caused by 1d0a7e77

src/cli/index.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
-import { version } from "package.json";
+import { version } from "../../package.json";
 
 import Excel from "exceljs";
-import ExcelJSWriter from "src/workbook/exceljs-writer";
+import ExcelJSWriter from "../../src/workbook/exceljs-writer";
 
-import OCX from "src";
+import OCX from "../../src";
 
 import * as pipeline from "./pipeline-steps";
 