19f4267e25c123689f5948c89146543969e92d3a
Patrick Johnmeyer <patrick@ltse.com>
2023-03-14 12:41:44 -0500

    Add OutstandingStockSharesCalculator

--- src/model/calculations.ts
+++ src/model/calculations.ts
@@ -8,6 +8,21 @@ function convertRatioToDecimalNumber(ratio: {
   return quotient;
 }
 
+class OutstandingStockSharesCalculator {
+  private value_: Big = Big("0");
+
+  public get value(): number {
+    return this.value_.toNumber();
+  }
+
+  public apply(txn: { object_type: string; quantity?: string }) {
+    if (txn.quantity) {
+      this.value_ = this.value_.plus(txn.quantity);
+    }
+  }
+}
+
 export default {
   convertRatioToDecimalNumber,
+  OutstandingStockSharesCalculator,
 };
---
+++ test/model/calculations.test.ts
@@ -0,0 +1,17 @@
+import Calculations from "src/model/calculations";
+
+import { describe, expect, test } from "@jest/globals";
+
+describe(Calculations.OutstandingStockSharesCalculator, () => {
+  test("zero case", () => {
+    const subject = new Calculations.OutstandingStockSharesCalculator();
+    expect(subject.value).toBe(0);
+  });
+
+  test("issuances", () => {
+    const subject = new Calculations.OutstandingStockSharesCalculator();
+    subject.apply({ object_type: "TX_STOCK_ISSUANCE", quantity: "10" });
+    subject.apply({ object_type: "TX_STOCK_ISSUANCE", quantity: "1" });
+    expect(subject.value).toBe(11);
+  });
+});
