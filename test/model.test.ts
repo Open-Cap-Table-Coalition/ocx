93ff8715885d3a954c77e4a66fb10cb05a5e91fa
David Yao <yaodavidarmel@gmail.com>
2023-05-02 17:54:46 -0500

    removed unecessary code

--- test/model.test.ts
+++ test/model.test.ts
@@ -268,9 +268,6 @@ describe(OCX.Model, () => {
 
       model.consume(fakeStockIssuanceForStakeholder("joe", "Fake")[1]);
       model.consume(fakeStockIssuanceForStakeholder("joe", "Fake")[2]);
-      while (model.hasPendingTransactions()) {
-        model.consumePendingTransactions();
-      }
       expect(
         model.getStakeholderStockPlanHoldings(
           model.stakeholders[0],
